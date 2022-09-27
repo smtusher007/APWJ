8e0495898042556d5fafac861ac3012c62857a52
S M Tusher <85401497+smtusher007@users.noreply.github.com>
2022-09-27 10:33:26 +0600

    Age Calculator 1.001

---
+++ ageCalculator/src/Person.java
@@ -0,0 +1,31 @@
+
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String dob;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+}
